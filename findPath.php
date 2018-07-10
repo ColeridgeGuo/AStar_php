c9dbdc391dcd010e5c376c7c004ab02cea168ed2
Jack Guo <yingxuan.guo9771@gmail.com>
2018-07-10 15:48:25 -0400

    printPath stores the path in the database

---
+++ findPath.php
@@ -0,0 +1,35 @@
+<html>
+<head><title>Find the path</title></head>
+<body>
+
+<form method="POST" action="aStarPath.php">
+  <!-- todo: TO BE UNCOMMENTED
+  Please enter the latitude of the starting point:
+  <input type="text" name="startLat">
+  <br>
+  Please enter the longitude of the starting point:
+  <input type="text" name="startLon">
+  <br>
+
+  Please enter the building name of the destination:
+  <input type="text" name="building">
+  <br>
+  Please enter the room number of the destination:
+  <input type="text" name="room">
+  <br>
+  -->
+  Please enter the user ID:
+  <input type="text" name="userID">
+  <br>
+  Please enter the id of the starting point:
+  <input type="text" name="startID">
+  <br>
+  Please enter the id of the destination:
+  <input type="text" name="targetID">
+  <br>
+  <input type="submit" value="Submit">
+</form>
+<a href="../index.php">Return to Homepage.</a>
+
+</body>
+</html>
